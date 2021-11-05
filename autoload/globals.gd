7ab0b3fc87c0a685c185ae74ab4160330777c044
MaskedTrench <markushasorokin@yandex.ru>
2021-11-05 22:11:05 +0300

    Attemp To rework all of this
    Started rework from GUI main menu interface

--- autoload/globals.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-var damg_ind = preload("res://objects/damage_indicator.tscn")
-
-
-func damage_indicator(pos:Vector2, damage:int):
-	var new = damg_ind.instance()
-	get_tree().current_scene.add_child(new)
-	new.global_position = pos
-	new.init(damage)
--- objects/scripts/stun_label.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-func _process(delta):
-	position.y = lerp(position.y, -50, 0.1)
-	modulate.a = lerp(modulate.a, 0, 0.05)
-	if modulate.a < 0.01:
-		queue_free()
-	
