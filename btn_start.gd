0a1a3a316149953b799d257f77bfc4ba68a0edfb
MaskedTrench <markushasorokin@yandex.ru>
2021-11-05 10:09:26 +0300

    Update btn_start.gd

--- btn_start.gd
+++ btn_start.gd
@@ -3,12 +3,14 @@ extends Button
 func start_game(): 
 	print_debug("Changed Scene")
 	var music_node = get_node("/root/MusicController").get_node("AmbietMusic") # Getting Music Node
-	music_node.volume_db = lerp(int(music_node.volume_db), 0, 0.1) # Music volume decrease
-
+	music_node.volume_db = lerp(int(music_node.volume_db), -80, 0.3) # Music volume decrease
+	# Change Scene script
+	
 func _init(): 
-	# The Place where Errors Beggins
+	self.connect("pressed", self, "start_game") # Binding buttons press event on start_game func
+
+func _ready():
 	var music_node = get_node("/root/MusicController") # Music node
 	music_node = music_node.get_node("AmbietMusic") 
-	music_node.stream.set_stream(preload("res://HomeMenu.mp3")) # Music setter
-	# The Place where Errors Ends
-	self.connect("pressed", self, "start_game") # Binding buttons press event on start_game func
+	music_node.stream = preload("res://HomeMenu.mp3") # Music setter
+	music_node.playing = true
