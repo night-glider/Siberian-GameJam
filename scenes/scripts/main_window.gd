4e697e2aa2c4a544a18f4b4e8fb771178fad4ca7
MaskedTrench <markushasorokin@yandex.ru>
2021-11-06 14:12:40 +0300

    Restoring all again!

--- scenes/scripts/main_window.gd
+++ scenes/scripts/main_window.gd
@@ -1,30 +1,10 @@
 extends MarginContainer
 
 func _ready():
-	# Music node
-	var music_node = get_node("/root/MusicController").get_node("AmbietMusic")  # Music node
-	music_node.stream = preload("res://audio/HomeMenu.mp3") # Music setter
-	
-	# Settings window node
-	var settingsBar = get_node("Settings/VBoxContainer3/HBoxContainer/VBoxContainer2/MusicScrollBar")
-	
-	# Openned config file parser
-	var file = File.new()
-	file.open("res://savedata/configs.json", file.READ) # The default JSON file should be gitignored
-	var json = file.get_as_text()
-	var configs = JSON.parse(json).result
-	file.close()
-	
-	# Setting configs from configs.json
-	#settingsBar.value = float(configs["music"])
-	#music_node.volume_db = float(configs["music"]) - 100
-	
-	
+	pass
 	
 func _on_Start_pressed():
 	print_debug("Game started")
-	var music_node = get_node("/root/MusicController").get_node("AmbietMusic") # Getting Music Node
-	music_node.volume_db = lerp(int(music_node.volume_db), -80, 0.3) # Music volume decrease
 	# Change Scene script
 	get_tree().change_scene("res://locations/loc_home.tscn")
 
