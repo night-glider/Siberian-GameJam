2d10c800dde9fad7aaaca33b47ec6ccb1f4eeea0
0night-glider0 <nightgliderdev@gmail.com>
2021-11-06 21:55:38 +0300

    улучшение класса location. bounding box теперь рисуется

--- custom_nodes/location.gd
+++ custom_nodes/location.gd
@@ -1,15 +1,24 @@
+tool
 extends YSort
 class_name location
 
 export(Rect2) var bounding_box
 
 func _ready():
-	var check = File.new() 
-	var player_res
-	if check.file_exists("user://player.tscn"):
-		player_res = load("user://player.tscn")
-	else:
-		player_res = load("res://objects/player.tscn")
-	var player = player_res.instance()
-	player.position = get_node("spawn_point"+str(SceneChanger.spawnpoint)).position
-	add_child(player)
+	if not Engine.editor_hint:
+		var check = File.new() 
+		var player_res
+		if check.file_exists("user://player.tscn"):
+			player_res = load("user://player.tscn")
+		else:
+			player_res = load("res://objects/player.tscn")
+		var player = player_res.instance()
+		player.position = get_node("spawn_point"+str(SceneChanger.spawnpoint)).position
+		add_child(player)
+
+func _process(delta):
+	update()
+
+func _draw():
+	if Engine.editor_hint:
+		draw_rect( bounding_box, Color.red, false, 20)
